20281bf87ab859d0caad0546a73bee5ebc635e5e
hassanbaryal <h.baryal2@gmail.com>
2022-04-12 14:16:30 -0400

    Digit and operator buttons are now functional

--- script.js
+++ script.js
@@ -3,9 +3,15 @@
 
 const topScreen = document.querySelector('.top-screen')
 const bottomScreen = document.querySelector('.bottom-screen')
+let number1 = '';
+let number2 = '';
+let answer;
+let operator = [];
+let nextOperator
+let firstOperatorClicked = false
 
 function add(x, y) {
-    return x + y
+    return parseFloat(x) + parseFloat(y)
 }
 
 function subctract(x, y) {
@@ -22,24 +28,97 @@ function divide(x, y) {
 
 
 function operate (operator, x, y) {
-    return
+    if (operator == '+') {
+        return add(x, y)
+    } else if (operator == '-') {
+        return subctract(x, y)
+    } else if (operator == '×') {
+        return multiply(x, y)
+    } else {
+        return divide(x, y)
+    }
 }
 
 
-function updateScreen (e) {
-    bottomScreen.textContent += e.target.textContent
+function beginCalculations (operator, x, y) {
+    let userInput = bottomScreen.textContent
+    console.log(userInput)
+}
+
+function updateDisplay (answer = undefined) {
+    if (number2 == '') {
+        topScreen.textContent = `${number1+operator}`
+        bottomScreen.textContent = ''
+        console.log('apple')
+    } else {
+        answer = operate(operator[0], number1, number2)
+        operator.splice(0,1)
+        number1 = answer
+        topScreen.textContent = number1+operator[0]
+        bottomScreen.textContent = ''
+        number2 = ''
+    }
+
+
 }
 
 const equalBtn = document.querySelector('.equal')
 
-// equalBtn.addEventListener('click')
+equalBtn.addEventListener('click', beginCalculations)
 
 
-const buttons = document.querySelectorAll('.character')
-const buttonsArray = Array.from(buttons)
+const digitBtns = document.querySelectorAll('.digit')
+const digitBtnsArray = Array.from(digitBtns)
 
-buttonsArray.forEach((button) => {
-    button.addEventListener('click', function (e) {
-        updateScreen(e)
+digitBtnsArray.forEach((btn) => {
+    btn.addEventListener('click', function (e) {
+        // if (firstOperatorClicked) {
+        //     number2 += e.target.textContent
+        // } else number1 += e.target.textContent
+
+        bottomScreen.textContent += e.target.textContent
     } )
+})
+
+
+const operatorBtns = document.querySelectorAll('.operator')
+const operatorBtnsArray = Array.from(operatorBtns)
+
+operatorBtnsArray.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+
+        operator.push(e.target.textContent)
+
+        if (!firstOperatorClicked) {
+            number1 = bottomScreen.textContent
+            if(number1 == '') number1 = 0
+        } else {
+            number2 = bottomScreen.textContent
+            if (number2 == '') number2 = 0
+        }
+
+        console.log(number1 + '|' + number2 + '|' + operator[0])
+        updateDisplay()
+
+        firstOperatorClicked = true
+
+        // if (number1 == '') { number1 = 0 }
+        // else if (number2 == '') { number2 = 0}
+
+        
+        // if (number1 == undefined) {
+        //     if (bottomScreen.textContent == '') {
+        //         number1 = 0
+        //     } else number1 = bottomScreen.textContent
+        // } else {
+        //     if (bottomScreen.textContent == '') {
+        //         number2 = 0
+        //     } else number2 = bottomScreen.textContent
+
+        //     beginCalculations(operator, number1, number2)
+        // }
+
+        
+        
+    })
 })
